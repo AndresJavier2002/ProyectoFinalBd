4e0287a820075e9734650fdabb9104c135c5ca27
AndresJavier2002 <150830556+AndresJavier2002@users.noreply.github.com>
2023-11-20 16:45:38 -0500

    Update Producto.java

--- CarritoDeComprasBd/src/java/com/modelo/Producto.java
+++ CarritoDeComprasBd/src/java/com/modelo/Producto.java
@@ -1,14 +1,76 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.modelo;
 
-/**
- *
- * @author DELL
- */
+import java.io.InputStream;
+
 public class Producto {
+    int id_producto;
+    String nombre;
+    InputStream foto;
+    String dercripcion;
+    double precio;
+    int stock;
+
+    public Producto() {
+    }
+
+    public Producto(int id_producto, String nombre, InputStream foto, String dercripcion, double precio, int stock) {
+        this.id_producto = id_producto;
+        this.nombre = nombre;
+        this.foto = foto;
+        this.dercripcion = dercripcion;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public int getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(int id) {
+        this.id_producto = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public InputStream getFoto() {
+        return foto;
+    }
+
+    public void setFoto(InputStream foto) {
+        this.foto = foto;
+    }
+
+    public String getDercripcion() {
+        return dercripcion;
+    }
+
+    public void setDercripcion(String dercripcion) {
+        this.dercripcion =dercripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    
     
 }

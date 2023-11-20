837267cceff95af7e27cb6b7c1f8d8ee2841f0b5
AndresJavier2002 <150830556+AndresJavier2002@users.noreply.github.com>
2023-11-20 16:44:19 -0500

    Update Controlador.java

--- CarritoDeComprasBd/src/java/com/controlador/Controlador.java
+++ CarritoDeComprasBd/src/java/com/controlador/Controlador.java
@@ -1,7 +1,11 @@
 package com.controlador;
 
+import com.modelo.Producto;
+import com.modelo.ProductoEntregado;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,22 +13,19 @@ import javax.servlet.http.HttpServletResponse;
 
 
 public class Controlador extends HttpServlet {
+    
+    ProductoEntregado pegd =new ProductoEntregado();
+    List<Producto> productos= new ArrayList<>();
+    
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+       
         response.setContentType("text/html;charset=UTF-8");
         try ( PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
+           out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
             out.println("<title>Servlet Controlador</title>");            
@@ -34,45 +35,40 @@ public class Controlador extends HttpServlet {
             out.println("</body>");
             out.println("</html>");
         }
+    
+    String accion = request.getParameter("accion");
+    productos = pegd.listar();
+    switch (accion){
+        case "AgregarCarrito":
+            int idp=Integer.parseInt(request.getParameter("id_producto"));
+            
+            break;
+        case  "Carrito":
+            
+            break;
+        default:
+            request.setAttribute("productos", productos);
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+            
     }
+    }      
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
 
 }
+
